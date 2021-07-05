f27215252cfefe2882651e4d6fd185821a96c812
Jakllp <57708725+Jakllp@users.noreply.github.com>
2021-07-05 14:46:42 +0200

    Implemented BarAPI into MarioKart - 1.17 ready
    New:
    - BarAPI is no longer a dependency as it is baked into MarioKart now
    - 1.17 is therefore supported

--- src/main/java/net/stormdev/barapi_1_17/nms/v1_8.java
+++ src/main/java/net/stormdev/barapi_1_17/nms/v1_8.java
@@ -1,100 +1,17 @@
-package net.stormdev.barapi_1_8;
+package net.stormdev.barapi_1_17.nms;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 
-import net.stormdev.mario.mariokart.MarioKart;
-
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 
-/**
-* This is the FakeDragon class for BarAPI.
-* It is based on the code by SoThatsIt.
-*
-* http://forums.bukkit.org/threads/tutorial-utilizing-the-boss-health-bar.158018/page-5#post-2053705
-*
-* @author James Mortemore
-*/
+import net.stormdev.barapi_1_17.Util;
 
 public class v1_8 extends FakeDragon {
 	private Object dragon;
 	private int id;
-	private static final String DRAGON_META = "bossBar";
-	private static final String BOSS_TEXT_META = "bossTextMeta";
 
-	public static void setDragon(Player player, FakeDragon dragon){
-		BarAPI1_8.players.put(player.getUniqueId(), dragon);
-		if(player.hasMetadata(DRAGON_META)){
-			clearDragon(player);
-		}
-		player.setMetadata(DRAGON_META, new MetadataValue(dragon, MarioKart.plugin));
-	}
-	
-	public static void start1_8PacketHandling(){
-		/*
-		Bukkit.getScheduler().runTaskTimerAsynchronously(BarAPI.plugin, new Runnable(){
-
-			@Override
-			public void run() {
-				for(Player player: new ArrayList<Player>(Arrays.asList(Bukkit.getOnlinePlayers()))){
-					if(player.hasMetadata(DRAGON_META)){
-						try {
-							String prevMsg = "";
-							if(player.hasMetadata(BOSS_TEXT_META)){
-								try {
-									Object o = player.getMetadata(BOSS_TEXT_META).get(0).value();
-									prevMsg = o.toString();
-								} catch (Exception e) {
-									player.removeMetadata(BOSS_TEXT_META, BarAPI.plugin);
-								}
-							}
-							
-							FakeDragon dragon = (FakeDragon) player.getMetadata(DRAGON_META).get(0).value();
-							if(dragon.name != prevMsg){
-								player.removeMetadata(BOSS_TEXT_META, BarAPI.plugin);
-								player.setMetadata(BOSS_TEXT_META, new MetadataValue(dragon.name, BarAPI.plugin));
-								player.sendMessage(ChatColor.GRAY+"[INFO] "+dragon.name);
-							}
-						} catch (Exception e) {
-							clearDragon(player);
-						}
-					}
-				}
-				return;
-			}}, 1l, 1l);
-			*/
-		Bukkit.getScheduler().runTaskTimerAsynchronously(MarioKart.plugin, new Runnable(){
-
-			@Override
-			public void run() {
-				
-				for(Player player: new ArrayList<Player>(Bukkit.getOnlinePlayers())){
-					if(/*!Versioning.is1_8(player)*/false){
-						continue; //Don't do anything
-					}
-						
-					if(BarAPI1_8.players.containsKey(player.getUniqueId())){
-						try {
-							FakeDragon dragon = (FakeDragon) BarAPI1_8.players.get(player.getUniqueId());
-							String msg = dragon.name;
-							Subtitle.sendSubtitle(player, msg);
-						} catch (Exception e) {
-							clearDragon(player);
-						}
-					}
-				}
-				return;
-			}}, 1l, 1l);
-	}
-	
-	public static void clearDragon(Player player){
-		player.removeMetadata(DRAGON_META, MarioKart.plugin);
-	}
-	
 	public v1_8(String name, Location loc) {
 		super(name, loc);
 	}
@@ -105,14 +22,15 @@ public class v1_8 extends FakeDragon {
 		Class<?> EntityLiving = Util.getCraftClass("EntityLiving");
 		Class<?> EntityEnderDragon = Util.getCraftClass("EntityEnderDragon");
 		Object packet = null;
+		
 		try {
 			dragon = EntityEnderDragon.getConstructor(Util.getCraftClass("World")).newInstance(getWorld());
-			
+
 			Method setLocation = Util.getMethod(EntityEnderDragon, "setLocation", new Class<?>[] { double.class, double.class, double.class, float.class, float.class });
 			setLocation.invoke(dragon, getX(), getY(), getZ(), getPitch(), getYaw());
-			
+
 			Method setInvisible = Util.getMethod(EntityEnderDragon, "setInvisible", new Class<?>[] { boolean.class });
-			setInvisible.invoke(dragon, isVisible());
+			setInvisible.invoke(dragon, true);
 
 			Method setCustomName = Util.getMethod(EntityEnderDragon, "setCustomName", new Class<?>[] { String.class });
 			setCustomName.invoke(dragon, name);
@@ -182,7 +100,7 @@ public class v1_8 extends FakeDragon {
 		Class<?> DataWatcher = Util.getCraftClass("DataWatcher");
 
 		Class<?> PacketPlayOutEntityMetadata = Util.getCraftClass("PacketPlayOutEntityMetadata");
-		
+
 		Object packet = null;
 		try {
 			packet = PacketPlayOutEntityMetadata.getConstructor(new Class<?>[] { int.class, DataWatcher, boolean.class }).newInstance(id, watcher, true);
@@ -206,55 +124,22 @@ public class v1_8 extends FakeDragon {
 	@Override
 	public Object getTeleportPacket(Location loc) {
 		Class<?> PacketPlayOutEntityTeleport = Util.getCraftClass("PacketPlayOutEntityTeleport");
-		
 		Object packet = null;
-		
+
 		try {
-			packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360), isVisible());
+			packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360), false);
 		} catch (IllegalArgumentException e) {
-			//e.printStackTrace();
+			e.printStackTrace();
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-			//e.printStackTrace();
+			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-			//e.printStackTrace();
+			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
-			//e.printStackTrace();
-		}
-		
-		if(packet == null){
-			try {
-				packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360));
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				e.printStackTrace();
-			} catch (NoSuchMethodException e) {
-				try {
-					packet = PacketPlayOutEntityTeleport.getConstructor(new Class<?>[] { int.class, int.class, int.class, int.class, byte.class, byte.class, boolean.class, boolean.class }).newInstance(this.id, loc.getBlockX() * 32, loc.getBlockY() * 32, loc.getBlockZ() * 32, (byte) ((int) loc.getYaw() * 256 / 360), (byte) ((int) loc.getPitch() * 256 / 360), isVisible(), true);
-				} catch (IllegalArgumentException e1) {
-					e1.printStackTrace();
-				} catch (SecurityException e1) {
-					e1.printStackTrace();
-				} catch (InstantiationException e1) {
-					e1.printStackTrace();
-				} catch (IllegalAccessException e1) {
-					e1.printStackTrace();
-				} catch (InvocationTargetException e1) {
-					e1.printStackTrace();
-				} catch (NoSuchMethodException e1) {
-					e1.printStackTrace();
-				}
-			}
+			e.printStackTrace();
 		}
 
 		return packet;
@@ -270,31 +155,26 @@ public class v1_8 extends FakeDragon {
 			watcher = DataWatcher.getConstructor(new Class<?>[] { Entity }).newInstance(dragon);
 			Method a = Util.getMethod(DataWatcher, "a", new Class<?>[] { int.class, Object.class });
 
-			a.invoke(watcher, 0, isVisible() ? (byte) 0 : (byte) 0x20);
+			a.invoke(watcher, 5, isVisible() ? (byte) 0 : (byte) 0x20);
 			a.invoke(watcher, 6, (Float) health);
 			a.invoke(watcher, 7, (Integer) 0);
 			a.invoke(watcher, 8, (Byte) (byte) 0);
 			a.invoke(watcher, 10, name);
 			a.invoke(watcher, 11, (Byte) (byte) 1);
 		} catch (IllegalArgumentException e) {
-
 			e.printStackTrace();
 		} catch (SecurityException e) {
-
 			e.printStackTrace();
 		} catch (InstantiationException e) {
-
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
-
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
-
 			e.printStackTrace();
 		} catch (NoSuchMethodException e) {
-
 			e.printStackTrace();
 		}
+
 		return watcher;
 	}
 }
--- src/main/java/net/stormdev/mario/server/BossBar.java
+++ src/main/java/net/stormdev/mario/server/BossBar.java
@@ -1,29 +1,16 @@
 package net.stormdev.mario.server;
 
-import me.confuser.barapi.BarAPI;
-import net.stormdev.mario.mariokart.MarioKart;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import net.stormdev.barapi_1_17.BarAPI;
+import net.stormdev.mario.mariokart.MarioKart;
 
 /*
  * Include BarAPI from 1.8_BarAPI with refactored packaged and if not found, use that
  * 
  */
 public class BossBar {
-	
-	private static boolean a(){
-		
-		boolean found = Bukkit.getPluginManager().getPlugin("BarAPI")!=null;
-		try {
-			Class.forName("me.confuser.barapi.BarAPI");
-			found = true;
-		} catch (ClassNotFoundException e) {
-			found = false;
-		}
-		return !found; //Return true if no BarAPI
-	}
-	
 	private static void execAsync(Runnable run){
 		if(Bukkit.isPrimaryThread()){
 			run.run();
@@ -34,113 +21,41 @@ public class BossBar {
 	}
 	
 	public static void setMessage(final Player player, final String message){
-		if(a()){
-			execAsync(new Runnable(){
-
-				@Override
-				public void run() {
-					net.stormdev.barapi_1_8.BarAPI1_8.setMessage(player, message);
-					return;
-				}});
+		execAsync( () -> {
+			BarAPI.setMessage(player, message);
 			return;
-		}
-		
-		execAsync(new Runnable(){
-
-			@Override
-			public void run() {
-				BarAPI.setMessage(player, message);
-				return;
-			}});
+		});
 	}
 	
 	public static void setMessage(final Player player, final String message, final float percent){
-		if(a()){
-			execAsync(new Runnable(){
-
-				@Override
-				public void run() {
-					net.stormdev.barapi_1_8.BarAPI1_8.setMessage(player, message, percent);
-					return;
-				}});
+		execAsync( () -> {
+			BarAPI.setMessage(player, message, percent);
 			return;
-		}
-		
-		execAsync(new Runnable(){
-
-			@Override
-			public void run() {
-				BarAPI.setMessage(player, message, percent);
-				return;
-			}});
+		});
 	}
 	
 	public static void setMessage(final Player player, final String message, final int seconds){
-		if(a()){
-			execAsync(new Runnable(){
-
-				@Override
-				public void run() {
-					net.stormdev.barapi_1_8.BarAPI1_8.setMessage(player, message, seconds);
-					return;
-				}});
+		execAsync( () -> {
+			BarAPI.setMessage(player, message, seconds);
 			return;
-		}
-
-		execAsync(new Runnable(){
-
-			@Override
-			public void run() {
-				BarAPI.setMessage(player, message, seconds);
-				return;
-			}});
+		});
 	}
 	
 	public static boolean hasBar(Player player){
-		if(a()){return net.stormdev.barapi_1_8.BarAPI1_8.hasBar(player);};
 		return BarAPI.hasBar(player);
 	}
 	
 	public static void removeBar(final Player player){
-		if(a()){
-			execAsync(new Runnable(){
-
-				@Override
-				public void run() {
-					net.stormdev.barapi_1_8.BarAPI1_8.removeBar(player);
-					return;
-				}});
+		execAsync( () -> {
+			BarAPI.removeBar(player);
 			return;
-		}
-		
-		execAsync(new Runnable(){
-
-			@Override
-			public void run() {
-				BarAPI.removeBar(player);
-				return;
-			}});
-		
+		});		
 	}
 	
 	public static void setHealth(final Player player, final float percent){
-		if(a()){
-			execAsync(new Runnable(){
-
-				@Override
-				public void run() {
-					net.stormdev.barapi_1_8.BarAPI1_8.setHealth(player, percent);
-					return;
-				}});
+		execAsync( () -> {
+			BarAPI.setHealth(player, percent);
 			return;
-		}
-		
-		execAsync(new Runnable(){
-
-			@Override
-			public void run() {
-				BarAPI.setHealth(player, percent);
-				return;
-			}});
+		});
 	}
 }
